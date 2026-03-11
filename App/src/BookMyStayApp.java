19cfea7becc7cf2731cb0b427b03a74673c9bbc5
sreemaannn <sreemanmr15@gmail.com>
2026-03-11 09:17:19 +0530

    Sreeman Added UC1-Welcome Page

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,7 @@
+public class BookMyStayApp {
+    public  static void main(String[] args){
+        System.out.println("Welcome To Hotel Booking Management System ");
+        System.out.println("Version 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
